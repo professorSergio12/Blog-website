d6aeec90f7b050b2fe070d34f66515a3589b88b1
Rohit Rawat <rohitrawat12092002@gmail.com>
2024-08-28 16:30:10 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(3000, ()=>{
-    console.log("Server is running");
-})
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
