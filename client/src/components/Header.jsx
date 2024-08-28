adee1a56c05b1b717e188d37d94f719b22786ac0
Rohit Rawat <rohitrawat12092002@gmail.com>
2024-08-28 15:51:50 +0530

    create a server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
         </Button>
 
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" pill>
+          <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
@@ -46,15 +46,15 @@ export default function Header() {
       </div>
 
       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
 
-        <Navbar.Link active={path === "/about"} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
 
-        <Navbar.Link active={path === "/projects"} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
