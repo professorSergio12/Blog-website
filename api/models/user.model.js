0817ee92875019346c428fcf380d567eb40e399a
Rohit Rawat <rohitrawat12092002@gmail.com>
2024-08-31 11:51:11 +0530

    add dark-light mode theme

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchmema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -19,11 +19,16 @@ const userSchmema = new mongoose.Schema(
     profilePicture: {
       type: String,
       default:
-        "https://imgs.search.brave.com/o8bDpgPcy9NJ_jxkUoCg6lFSNynwecFGT-7lQUAY3WY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzc0LzAzLzI0/LzM2MF9GXzE3NDAz/MjQ0M19GbGx5Rm9m/RlpqN0pPUFhHSmw3/NVVpRUVlSXE4QUll/Ry5qcGc",
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchmema);
+const User = mongoose.model("User", userSchema);
+
 export default User;
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -7,14 +7,14 @@ import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
-  const auth = getAuth(app);
+  const auth = getAuth(app);//initializes Firebase authentication for the current app instance. This auth object will be used to manage authentication, such as signing in users.
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleGoogleClick = async () => {
-    const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({ prompt: "select_account" });
+    const provider = new GoogleAuthProvider(); // creates a new instance of the Google Auth provider, which is used to handle Google sign-in.
+    provider.setCustomParameters({ prompt: "select_account" }); 
     try {
-      const resultsFromGoogle = await signInWithPopup(auth, provider);
+      const resultsFromGoogle = await signInWithPopup(auth, provider); //opens a popup window for the user to sign in with their Google account. If successful, it returns the user's Google account information, which is stored in resultsFromGoogle.
       // console.log(resultsFromGoogle);
 
       const res = await fetch("/api/auth/google", {
