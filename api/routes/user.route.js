247c61be0956f80ad3a819fa777cd5097eda4656
Rohit Rawat <rohitrawat12092002@gmail.com>
2024-09-04 21:37:36 +0530

    complete sign out functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,11 +4,13 @@ import {
   test,
   updateUser,
   deleteUser,
+  signout,
 } from "../constollers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
 
 export default router;
