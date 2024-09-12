8326adc5b6e91c482ae2789ebb11c3b97bb7f002
Rohit Rawat <rohitrawat12092002@gmail.com>
2024-09-12 18:13:25 +0530

    add dashboard overview

--- client/src/components/DashSlidebar.jsx
+++ client/src/components/DashSlidebar.jsx
@@ -47,6 +47,18 @@ export default function DashSlidebar() {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
+          {/* DASHBOARD */}
+          {currentUser && currentUser.isAdmin && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item
+                active={tab === "dash" || !tab}
+                icon={HiChartPie}
+                as="div"
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           {/* PROFILE */}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
