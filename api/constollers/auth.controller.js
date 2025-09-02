fc013b9cda622a5b46c32fdf79c409541f57a27f
Rohit Rawat <107769619+professorSergio12@users.noreply.github.com>
2025-09-02 22:24:57 +0530

    Update auth.controller.js

--- api/constollers/auth.controller.js
+++ api/constollers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
