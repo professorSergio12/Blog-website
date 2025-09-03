d2a4b8023ce5d767222fc7c01e678f7ac2892819
Rohit Rawat <107769619+professorSergio12@users.noreply.github.com>
2025-09-03 15:25:12 +0530

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,20 @@
 import express from "express";
 const router = express.Router();
 
-import { signup, signin, google } from "../constollers/auth.controller.js";
+import {
+  signup,
+  signin,
+  google,
+  forgetPassword,
+  verifyOtp,
+  resetPassword,
+} from "../constollers/auth.controller.js";
 
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.post('/google', google);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/google", google);
+router.post("/forget-password", forgetPassword);
+router.post("/verify-otp", verifyOtp);
+router.post("/reset-password", resetPassword);
 
 export default router;
